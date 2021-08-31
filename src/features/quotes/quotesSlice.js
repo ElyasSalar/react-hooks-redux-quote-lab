4ca8faa6d4f0af86e4803e5eb841259a94857b78
Elyas <elyassalar8@gmail.com>
2021-08-31 15:59:05 +0300

    54th assignment solution

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,55 @@
 // Action Creators
-// TODO: Create action creators as defined in tests
+export function addQuote(quote){
+  return {
+    type: "quotes/add",
+    payload: quote
+  }
+}
+
+export function removeQuote(id){
+  return {
+    type: "quotes/remove",
+    payload: id
+  }
+}
+
+export function upvoteQuote(id){
+  return {
+    type: "quotes/upvote",
+    payload: id
+  }
+}
+
+export function downvoteQuote(id){
+  return {
+    type: "quotes/downvote",
+    payload: id
+  }
+}
 
 // Reducer
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
-  return state;
+  switch(action.type){
+    case "quotes/add":
+      return [...state, action.payload]
+    case "quotes/remove":
+      const withoutDeleted = state.filter(quote => quote.id !== action.payload);
+      return withoutDeleted
+    case "quotes/upvote":
+      const upvoted = state.map(quote => {
+        if(quote.id === action.payload) return {...quote, votes: quote.votes + 1};
+        return quote;
+      })
+      return upvoted;
+    case "quotes/downvote":
+      const downvoted = state.map(quote => {
+        if(quote.id === action.payload && quote.votes > 0) return {...quote, votes: quote.votes - 1};
+        return quote;
+      })
+      return downvoted;
+    default:
+      return state;
+  }
 }
